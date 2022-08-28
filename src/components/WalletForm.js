2822ee6ca770a2c8c37a1c069af9380d78d8bd3e
Pedro Ayres <pedroayresb@gmail.com>
2022-08-28 00:12:13 -0300

    Dessa vez vai?

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { fetchCurrencies, saveExpense } from '../redux/actions/index';
-import '../styles/WalletFormStyle.css';
 
 class WalletForm extends Component {
   constructor() {
