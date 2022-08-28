254e1a86acb22cbecd4d752fad35584d9d2588bb
Pedro Ayres <pedroayresb@gmail.com>
2022-08-27 21:38:21 -0300

    Conserta Linter 2

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Expense from './Expense';
-import '../styles/TableStyle.css';
 
 class Table extends Component {
   render() {
